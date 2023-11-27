c7b53eae0fdc0f02ccc45117799214c21ac45997
Roberto Quintero <langdonhp435@gmail.com>
2023-11-27 12:43:15 -0600

    Corrige direccion IP

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -5,6 +5,11 @@ const uploadImage=(req=request,res=response)=>{
 
   const name=String(req.body.name.split('uploads/')[1]) 
   const directoryPath = __dirname.split('controllers')[0]+'uploads/'+name ;
+  
+  // console.log(req.file.filename)
+  // console.log(name)
+  // return
+
   if(name!==req.file.filename){
     fs.unlink(directoryPath , (err) => {
       console.log("Delete File successfully.");
@@ -15,17 +20,13 @@ const uploadImage=(req=request,res=response)=>{
       ok:true,
       data:`${process.env.BASE_URL}:${process.env.PORT}/uploads/${req.file.filename}`
     })
-    
   } catch (error) {
     console.log({error})
     return res.status(400).json({
       ok:false,
       data:'Error al subir imagen'
-    })
-    
+    })  
   }
-
-  
 }
 
 module.exports={
